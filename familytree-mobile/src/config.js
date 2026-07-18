3a434999b532acd6fd04601086779ed7505a0e8b
Nurasyl555 <kalmahanbet.nur06@gmail.com>
2026-07-18 13:04:53 +0500

    find and fix bug

--- familytree-mobile/src/config.js
+++ familytree-mobile/src/config.js
@@ -1,10 +1,10 @@
 import { Platform } from 'react-native';
 
-const LOCAL_IP = '172.20.10.3';
+const LOCAL_IP = '192.168.0.133';
 
 export const API_BASE_URL =
   Platform.OS === 'android'
-    ? `http://172.20.10.3:8000/api`
-    : `http://172.20.10.3:8000/api`;
+    ? `http://${LOCAL_IP}:8000/api`
+    : `http://${LOCAL_IP}:8000/api`;
 
 export default API_BASE_URL;
